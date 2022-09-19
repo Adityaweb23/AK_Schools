6463f5a259018221a709b9c27795722717a54d34
Adityaweb23 <74668650+Adityaweb23@users.noreply.github.com>
2022-09-19 11:00:31 +0530

    images commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { BsCheck2Circle, BsCheckCircleFill,BsCheckCircle } from "react-icons/bs";
+import { BsCheck2Circle, BsCheckCircleFill, BsCheckCircle } from "react-icons/bs";
 import png1 from '../newpng2.png'
+import png2 from '../newpng3.png'
+import png3 from '../newpng4.png'
 const About = () => {
 
   return (
@@ -25,16 +27,15 @@ const About = () => {
               data-aos-duration="1000"
             >
               <div class="row align-items-center">
-                <div class="row">
-                  <div class="col-lg-10 col-md-10 mt-2 pt-2">
-                    <div class="card work-desk rounded border-0 shadow-lg overflow-hidden">
-                      <img
-                        src="https://www.shutterstock.com/image-vector/happy-parents-children-celebrating-success-600w-1733827256.jpg"
-                        class="img-fluid"
-                        alt="Image"
-                      />
-                      <div class="img-overlay bg-dark"></div>
-                    </div>
+                <div class="row ms-auto">
+                  <div class="col-lg-10 col-md-10 col-12 mt-2 pt-2 ">
+
+                    <img
+                      src={png1}
+                      class="img-fluid h-100 w-75"
+                      alt="Image"
+                    />
+
                   </div>
                 </div>
               </div>
@@ -45,7 +46,7 @@ const About = () => {
               data-aos="zoom-out-up"
               data-aos-duration="1000"
             >
-              <div class="section_title ml-lg-5">
+              <div class="section_title ms-lg-5">
                 <br />
                 <br />
                 <h4 class="title mb-4" style={{ fontSize: 29 }}>
@@ -54,19 +55,19 @@ const About = () => {
 
                 <p class=" mb-0">
                   To be a leader in school Education and Daycare, provide children “a home-away-from-home“ to give them “best-in-class” environment to optimize their Potential.</p>
-                  <br />
+                <br />
                 <h4 class="title mb-4" style={{ fontSize: 29 }}>
-                Our Mission
+                  Our Mission
                 </h4>
                 <p>
-                "To help young minds grow in a child-friendly environment for the holistic development of a child." We focus on the satisfaction of the child's inquisitive mind through concept clarity. By infusing love and care into every aspect of learning, we, at Blue Swan, convert every day into wonderful learning experiences that lead to the all-round development of the child. Moreover, here the child gets immense happiness, complete freedom and the required guidance to grow and blossom in the happy world of BlueSwan!
+                  "To help young minds grow in a child-friendly environment for the holistic development of a child." We focus on the satisfaction of the child's inquisitive mind through concept clarity. By infusing love and care into every aspect of learning, we, at Blue Swan, convert every day into wonderful learning experiences that lead to the all-round development of the child. Moreover, here the child gets immense happiness, complete freedom and the required guidance to grow and blossom in the happy world of BlueSwan!
                 </p>
               </div>
 
             </div>
           </div>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <div class="row align-items-center">
             <div
               class="col-lg-6 col-md-6 col-12 order-1 order-md-2"
@@ -77,13 +78,13 @@ const About = () => {
                 <br />
                 <br />
                 <h4 class="title mb-4 p-3" style={{ fontSize: 29 }}>
-                Awards And Achivements
+                  Awards And Achivements
                 </h4>
 
                 <p class=" mb-0 p-3">
-                By far, the most common end of the year award is for attendance. Students can be rewarded for each quarter, semester, or for the entire Blue Ribbon from Dreamstime school year. This is a popular reward mainly because everyone is in with a chance of winning. Students do not need to have excelled in any kind of academic achievement; they just need to have attended school. Academic achievements are important to recognize. After all, students attend school primarily to learn new skills. Recognizing the achievements that they have made in each area with curricular awards is a great way to foster confidence and promote good study habits. The following are examples of popular end of year awards that are often used by elementary school teachers.</p>
-                  <br />
-               
+                  By far, the most common end of the year award is for attendance. Students can be rewarded for each quarter, semester, or for the entire Blue Ribbon from Dreamstime school year. This is a popular reward mainly because everyone is in with a chance of winning. Students do not need to have excelled in any kind of academic achievement; they just need to have attended school. Academic achievements are important to recognize. After all, students attend school primarily to learn new skills. Recognizing the achievements that they have made in each area with curricular awards is a great way to foster confidence and promote good study habits. The following are examples of popular end of year awards that are often used by elementary school teachers.</p>
+                <br />
+
               </div>
 
             </div>
@@ -94,23 +95,21 @@ const About = () => {
             >
               <div class="row align-items-center">
                 <div class="row">
-                  <div class="col-lg-10 col-md-10 mt-2 pt-2">
-                    <div class="card work-desk ms-auto rounded border-0 shadow-lg overflow-hidden">
-                      <img
-                        src={png1}
-                        class="img-fluid"
-                        alt="Image"
-                      />
-                      <div class="img-overlay bg-dark"></div>
-                    </div>
+                  <div class="col-lg-10 col-md-10 col-12 mt-2 pt-2 ">
+                    <img
+                      src={png3} 
+                      class="img-fluid h-75"
+                      alt="Image"
+                    />
+
                   </div>
                 </div>
               </div>
             </div>
 
           </div>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <div class="row align-items-center">
             <div
               class="col-lg-6 col-md-6 order-2 order-md-1 mt-sm-0 opt-sm-0"
@@ -118,16 +117,14 @@ const About = () => {
               data-aos-duration="1000"
             >
               <div class="row align-items-center">
-                <div class="row">
-                  <div class="col-lg-10 col-md-10 mt-2 pt-2">
-                    <div class="card work-desk rounded border-0 shadow-lg overflow-hidden">
-                      <img
-                        src="https://www.shutterstock.com/image-vector/happy-parents-children-celebrating-success-600w-1733827256.jpg"
-                        class="img-fluid"
-                        alt="Image"
-                      />
-                      <div class="img-overlay bg-dark"></div>
-                    </div>
+                <div class="row ">
+                  <div class="col-lg-10 col-md-10  col-12 mt-2 pt-2 ">
+                    <img
+                      src={png2} 
+                      class="img-fluid h-75"
+                      alt="Image"
+                    />
+
                   </div>
                 </div>
               </div>
@@ -142,27 +139,27 @@ const About = () => {
                 <br />
                 <br />
                 <h4 class="title mb-4" style={{ fontSize: 29 }}>
-                Why BlueSwan Public School
+                  Why BlueSwan Public School
                 </h4>
                 <ul>
-                     <li><BsCheckCircle />&nbsp;  Why BlueSwan Public School</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Spacious Ambitious</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Good ventilation &amp; Fresh air</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Positive learning environment for your child</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Full security</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Completely in residential area</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  The homely and warm atmosphere, which the children <br/>truly enjoy.</li>
-                     <br/>
-                     <li><BsCheckCircle />&nbsp;  Kids are allowed to freely move around. All<br/> the measures are taken so that the child’s safety is ensured.</li>
-                     <br/>
-                     <li><BsCheckCircle /> &nbsp; Utmost, care is taken to design the premises<br/> in such a way that the child feels its second home.</li>
-                  </ul>
+                  <li><BsCheckCircle />&nbsp;  Why BlueSwan Public School</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Spacious Ambitious</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Good ventilation &amp; Fresh air</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Positive learning environment for your child</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Full security</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Completely in residential area</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  The homely and warm atmosphere, which the children <br />truly enjoy.</li>
+                  <br />
+                  <li><BsCheckCircle />&nbsp;  Kids are allowed to freely move around. All<br /> the measures are taken so that the child’s safety is ensured.</li>
+                  <br />
+                  <li><BsCheckCircle /> &nbsp; Utmost, care is taken to design the premises<br /> in such a way that the child feels its second home.</li>
+                </ul>
               </div>
 
             </div>
